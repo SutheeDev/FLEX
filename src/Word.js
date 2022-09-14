fe504dab5110fc276969325222766acdb300c593
SU-T <guitaristpeet@gmail.com>
2022-09-14 15:27:17 -0700

    adjust ease

--- src/Word.js
+++ src/Word.js
@@ -12,58 +12,51 @@ const Word = () => {
             letterSpacing: "0px",
         }, {
             fontStretch: "100%",
-            duration: 0.4,
-            ease: 'power2.inOut'
+            duration: 0.6,
+            ease: 'power1.inOut'
         }, 0.4)
         wordTl.to(flex, {
             fontWeight: 500,
             duration: 0.15,
-            ease: 'power2.inOut',
+            ease: 'power1.inOut',
         }, "+=0.7")
         // Animate Variable CSS tageting html
         wordTl.to('html', {
             '--italic': 1,
             duration: 0.15,
-            ease: 'power2.inOut',
+            ease: 'power1.inOut',
         }, '-=0.15')
         wordTl.to(flex, {
             fontStretch: "20%",
             fontWeight: 900,
             letterSpacing: "-25px",
             duration: 0.25,
-            ease: 'power2.inOut',
+            ease: 'power1.inOut',
         }, "+=0.7")
         wordTl.to('html', {
             '--italic': 0,
             duration: 0.15,
-            ease: 'power2.inOut',
+            ease: 'power1.inOut',
         }, "+=0.7")
         wordTl.to(flex, {
             fontStretch: "170%",
             fontWeight: 100,
             letterSpacing: "0px",
             duration: 0.15,
-            ease: 'power2.inOut',
+            ease: 'power1.inOut',
         }, "-=0.15")
         wordTl.to(flex, {
             fontStretch: "0%",
             letterSpacing: '-30px',
             duration: 0.2,
-            ease: 'power2.inOut',
+            ease: 'power1.inOut',
         }, "+=0.7")
         wordTl.to(flex, {
             fontWeight: 200,
             letterSpacing: "0px",
             duration: 0.2,
-            ease: 'power2.inOut',
+            ease: 'power1.inOut',
         }, '+=0.7')
-
-
-
-
-
-
-
     }, []);
 
     return (
