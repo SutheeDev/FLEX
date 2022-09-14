de1f98c673bfd532bb8a399ab460d17213df9748
SU-T <guitaristpeet@gmail.com>
2022-09-14 15:39:42 -0700

    fix glich when reloading the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import Word from './Word'
+import {gsap} from 'gsap';
 
 const App = () => {
+  let container = useRef(null);
+
+  useEffect(() => {
+    gsap.to(container, 0, {css: {visibility: 'visible'}})
+  }, []);
+
   return (
-    <article className="square">
+    <article ref={el => container = el} className="container">
       <Word/>    
     </article>
   )
